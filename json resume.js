9043273cd89da22a1c9b955b648eb9a8f9ba32d7
krishnamk26 <127760812+krishnamk26@users.noreply.github.com>
2023-03-22 19:40:55 +0530

    Add files via upload

---
+++ json resume.js
@@ -0,0 +1,51 @@
+let myResume = {
+    BIODATA :{
+        NAME : 'KRISHNA MK',
+        EMAIL : 'krissurya1@gmail.com',
+        PHONE : '9578675765',
+        ADDRESS : {
+            DoorNo : '1/1',
+            Area : 'Krishnapuram colony',
+            City : 'madurai',
+            Pincode : '625014',
+        },
+    },
+    EDUCATION :{
+        SSLC :{
+            UNIVERSITY : 'SHSS',
+            YEAR : '2013',
+            PERCENTAGE : '78%',
+        },
+        HIGHERSECONDAY :{
+            UNIVERSITY : 'SHSS',
+            YEAR : '2015',
+            PERCENTAGE : '68%'
+        },
+        BACHELOROFDEGREE :{
+            UNIVERSITY : 'Kamarajar university',
+            YEAR : '2018',
+            PERCENTAGE : '50',
+        },
+    },
+    EXPERIENCE :{
+        QC :{
+            COMPANY : 'DIACRI TECH',
+            YEAR : '2018',
+        },
+        ASSOCIATE:{
+            COMPANY : 'SBL',
+            YEAR : '2019',
+        },
+        EXCUTIVE:{
+            COMPANY : 'RAMRAJ',
+            YEAR : '2023 - PRESENT',
+        },
+    },
+    SKILLS :[
+        'Java script',
+        'Good communication skills',
+        'Problem solving',
+        'Quick learing',
+    ],
+}
+console.log(myResume);
